ce28685a8d238a5257044676e466047d2ddfa414
blessingtb3 <blessingtb3@gmail.com>
2025-05-19 13:32:11 +0200

    fix: user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,10 @@
-namespace deliverableApi.models
+namespace DeliverableApi.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string userName { get; set; }
         public string password { get; set; }
+        public int role { get; set; } // 0 for user, 1 for admin
     }
 }
